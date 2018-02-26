be47bcf9b6418bc6e3246a66c4bc8b30f19141dc
MuhammadVT <mamatripik@gmail.com>
2018-02-26 14:13:40 -0500

    fix a bug related dimension mismatch

--- poes/get_aur_bnd.py
+++ poes/get_aur_bnd.py
@@ -499,11 +499,11 @@ class PoesAur(object):
                 equMltArr = currBndDF["equator_mlt"].values
                 # discard nan values
                 poleMlatArr = poleMlatArr[~numpy.isnan(poleMlatArr)]
-                poleMlonArr = poleMlonArr[~numpy.isnan(poleMlatArr)]
-                poleMltArr = poleMltArr[~numpy.isnan(poleMlatArr)]
+                poleMlonArr = poleMlonArr[~numpy.isnan(poleMlonArr)]
+                poleMltArr = poleMltArr[~numpy.isnan(poleMltArr)]
                 equMlatArr = equMlatArr[~numpy.isnan(equMlatArr)]
-                equMlonArr = equMlonArr[~numpy.isnan(equMlatArr)]
-                equMltArr = equMltArr[~numpy.isnan(equMlatArr)]
+                equMlonArr = equMlonArr[~numpy.isnan(equMlonArr)]
+                equMltArr = equMltArr[~numpy.isnan(equMltArr)]
                 # Concat the arrays together
                 latPoesAll = numpy.append( poleMlatArr, equMlatArr )
                 lonPoesAll = numpy.append( poleMlonArr, equMlonArr )
